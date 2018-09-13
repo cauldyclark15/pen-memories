5e01e1da11ed8adc111ad8ae46791ef781b5833f
Bryan M <janbryanmartirez@gmail.com>
2018-09-13 21:40:49 +0800

    added listen in server.js, added multer in app.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const local = require('../client/app')
+
 
 const app = express();
 
@@ -11,3 +13,11 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(3009, () => console.log('Listening at port 3009'));
+
+app.get('/', (req, res) => {
+    res.sendFile(htmlPath + '/index.html')
+})
+
+app.post('/upload', (req, res) => {
+    local.uploadCall(req,res)
+})
